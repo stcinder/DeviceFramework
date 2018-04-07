06389395bad6880b963393ec7c8b05a151473033
stcinder <38148142+stcinder@users.noreply.github.com>
2018-04-07 15:23:32 +1000

    Delete DeviceFramework_vers.d

--- Build/Intermediates.noindex/DeviceFramework.build/Debug/DeviceFramework.build/Objects-normal/x86_64/DeviceFramework_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/cinder/Cocoa/64Bit/Monitor\ Modules/DeviceFramework/Build/Intermediates.noindex/DeviceFramework.build/Debug/DeviceFramework.build/DerivedSources/DeviceFramework_vers.c

f4c976cefc16f68eaeba7035f9ee8961c620ef2d
stcinder <38148142+stcinder@users.noreply.github.com>
2018-04-07 15:26:24 +1000

    Delete DeviceFrameworkTests_vers.c

--- Build/Intermediates.noindex/DeviceFramework.build/Debug/DeviceFrameworkTests.build/DerivedSources/DeviceFrameworkTests_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char DeviceFrameworkTestsVersionString[];
- extern const double DeviceFrameworkTestsVersionNumber;
-
- const unsigned char DeviceFrameworkTestsVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:DeviceFrameworkTests  PROJECT:DeviceFramework-1" "\n";
- const double DeviceFrameworkTestsVersionNumber __attribute__ ((used)) = (double)1.;
